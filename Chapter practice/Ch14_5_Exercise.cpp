f9ae0c9fdb8f7939a339ab4d0fd630884e4e8561
Jonathan Steddom <jsteddom@asu.edu>
2026-09-08 11:04:05 -0700

    Chapter 14.5 exercise
    Worked through exercise in this chapter.

---
+++ Chapter practice/Ch14_5_Exercise.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Point3d 
+{
+private: 
+    int m_x { };
+    int m_y { };
+    int m_z { };
+public:
+    void setValues(int x, int y, int z) {
+        m_x = x;
+        m_y = y;
+        m_z = z;
+    }
+    // Note: Add const as values will not change for print, allows for const variables.s
+    void print() const {
+        std::cout << "<" << m_x << ", " << m_y << ", " << m_z <<">";
+    }
+    bool isEqual(const Point3d& pt) const {
+        if (m_x == pt.m_x && m_y == pt.m_y && m_z == pt.m_z) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+	Point3d point1{};
+	point1.setValues(1, 2, 3);
+
+	Point3d point2{};
+	point2.setValues(1, 2, 3);
+
+	std::cout << "point 1 and point 2 are" << (point1.isEqual(point2) ? "" : " not") << " equal\n";
+
+	Point3d point3{};
+	point3.setValues(3, 4, 5);
+
+	std::cout << "point 1 and point 3 are" << (point1.isEqual(point3) ? "" : " not") << " equal\n";
+
+	return 0;
+}
